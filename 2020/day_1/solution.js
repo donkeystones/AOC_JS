eb3c97f836f27519417d182762efcb8fb9e016ca
donkeystones <oscar.back96@outlook.com>
2024-05-22 16:32:45 +0200

    2020 day 1 part 2 start

--- 2020/day_1/solution.js
+++ 2020/day_1/solution.js
@@ -4,17 +4,21 @@ var data = fs.readFileSync('./input.txt','utf8');
 
 let data_arr = data.split("\n").map((item) => {return parseInt(item)});
 let part1 = 0;
+let part2 = 0;
 //-1 to skip the NaN at the end
 for(let i = 0; i < data_arr.length-1; i++) {
-	let sum = 2020;
-	sum -= data_arr[i];
+	let sum1 = 2020;
+	sum1 -= data_arr[i];
 
 	for(let j = i+1; j < data_arr.length-1; j++){
-		if(sum === data_arr[j]){
+		let sum2 = 2020 - data_arr[j];
+		if(sum1 === data_arr[j])
 			part1 = data_arr[j] * data_arr[i];
-			break;
+		for(let k = j+1;k < data_arr.length-1; k++){
+			if(sum2 === data_arr[k]) part2 = data_arr[i]*data_arr[j]*data_arr[k];
 		}
 	}
 }
 
 console.log(part1)
+console.log(part2);
