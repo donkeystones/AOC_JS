d40cebf0b03b861ff572e347e75d8cabe8f6b780
donkeystones <oscar.back96@outlook.com>
2024-05-22 10:04:47 +0200

    day 3 beginning

---
+++ 2022/day_3/solution.js
@@ -0,0 +1,10 @@
+let fs = require("fs");
+
+var data = fs.readFileSync('./input.txt','utf8');
+
+
+const priority = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//Fetch common item from compartments
+const fcifc = (rucksack) => {
+	
+}
