cdb10f0e53ee61e3b5d47efa60be6c9eec2b9d0a
donkeystones <oscar.back96@outlook.com>
2024-05-22 09:55:37 +0200

    Re-ReFixed part 1, done with part 2

--- 2022/day_2/solution.js
+++ 2022/day_2/solution.js
@@ -10,7 +10,7 @@ var data = fs.readFileSync('./input.txt','utf8');
 //Draw = 3
 //Lose = 0
 
-const get_match_outcome = (you, opponent) => {
+const get_match_outcome = (opponent, you) => {
 	if(you === "X" && opponent === "A") return 3;
 	else if(you === "X" && opponent === "B") return 0;
 	else if(you === "X" && opponent === "C") return 3;
@@ -29,6 +29,40 @@ const get_shape_score = (your_shape) => {
 	else return 3;
 }
 
+//Part 2
+//X = 0
+//Y = 3
+//Z = 8
+const get_match_outcome_part_2 = (opponent, you) => {
+	if(opponent === "A"){ //Rock
+		if(you === "X"){
+			return 0 + 3 //Scissor
+		}else if(you === "Y") {
+			return 3 + 1 //Rock
+		}else if(you === "Z"){
+			return 8 + 2 //Paper
+		}
+	}
+	else if (opponent === "B") { //Paper
+		if(you === "X"){
+			return 0 + 1 //Rock
+		}else if(you === "Y") {
+			return 3 + 2 //Paper
+		}else if(you === "Z"){
+			return 8 + 3 //Scissor
+		}
+	}
+	else if (opponent === "C") { //Scissor
+		if(you === "X"){
+			return 0 + 2 //Paper
+		}else if(you === "Y") {
+			return 3 + 3 //Scissor
+		}else if(you === "Z"){
+			return 8 + 1 //Rock
+		}
+	}
+}
+
 let total_part_1 = 0;
 let total_part_2 = 0;
 
@@ -42,19 +76,9 @@ data.split("\n").map((d) => {
 		match_part_1 = get_match_outcome(darr[0], darr[1]);
 		total_part_1 += shape + match_part_1;
 		//Part 2
-		
+		total_part_2 += get_match_outcome_part_2(darr[0],darr[1]);	
 	}
 });
 
 console.log(total_part_1);
-
-//Part 2
-//X = 0
-//Y = 3
-//Z = 8
-const get_match_outcome_part_2 = (outcome) => {
-	if(outcome === "X") return 0;
-	else if(outcome === "Y") return 3;
-	else return 8;
-}
-
+console.log(total_part_2);
