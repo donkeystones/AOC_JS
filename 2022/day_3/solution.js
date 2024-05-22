50fd9d4def3127b07ce8a6ca457dc4dfdfbe4605
donkeystones <oscar.back96@outlook.com>
2024-05-22 10:17:31 +0200

    day 3 part 1 done

--- 2022/day_3/solution.js
+++ 2022/day_3/solution.js
@@ -4,7 +4,25 @@ var data = fs.readFileSync('./input.txt','utf8');
 
 
 const priority = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 //Fetch common item from compartments
 const fcifc = (rucksack) => {
-	
+	let middle = Math.ceil(rucksack.length / 2);
+
+	let first = rucksack.slice(0,middle)
+	let second = rucksack.slice(middle)
+
+	return first.filter(item => second.includes(item));
 }
+
+const get_priority = (char) => {
+	return priority.indexOf(char) + 1;
+}
+
+let total_sum_part_1 = 0;
+data.split("\n").map(rs => {
+	if(rs !== "")
+		total_sum_part_1 += get_priority(fcifc(rs.split(""))[0]);
+});
+
+console.log(total_sum_part_1);
