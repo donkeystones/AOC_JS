ec9938f1eab0d50f69bd63f4695e753b4699ea87
donkeystones <oscar.back96@outlook.com>
2024-05-22 16:26:26 +0200

    2020 day 1 part 1 done

--- 2020/day_1/solution.js
+++ 2020/day_1/solution.js
@@ -2,4 +2,19 @@ let fs = require("fs");
 
 var data = fs.readFileSync('./input.txt','utf8');
 
-let data_arr = data.split("\n");
+let data_arr = data.split("\n").map((item) => {return parseInt(item)});
+let part1 = 0;
+//-1 to skip the NaN at the end
+for(let i = 0; i < data_arr.length-1; i++) {
+	let sum = 2020;
+	sum -= data_arr[i];
+
+	for(let j = i+1; j < data_arr.length-1; j++){
+		if(sum === data_arr[j]){
+			part1 = data_arr[j] * data_arr[i];
+			break;
+		}
+	}
+}
+
+console.log(part1)
