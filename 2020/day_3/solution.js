2b622744b08e30c765441e626230ff3a7d7d3483
donkeystones <oscar.back96@outlook.com>
2024-06-07 10:58:15 +0200

    2020 day 3 part 1 done

--- 2020/day_3/solution.js
+++ 2020/day_3/solution.js
@@ -2,13 +2,13 @@ let fs = require("fs");
 
 var data = fs.readFileSync('./input.txt','utf8');
 let data_arr = data.split("\n");
-let x = 3;
+let x = 0;
 let trees = 0;
 for(let y = 0; y < data_arr.length; y++){
 	if(data_arr[y+1] !== undefined){
-		if(data_arr[y+1][x] === "#") trees++;
 		x+= 3
-		if(x >= data_arr[y].length) x -= data_arr[y].length-1;
+		if(x >= data_arr[y].length) x -= data_arr[y].length;
+		if(data_arr[y+1][x] === "#") trees++;
 	}
 }
 console.log(trees);
