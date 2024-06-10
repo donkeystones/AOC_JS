d45ae6d735ff9acd682281a50baaf7063e60620c
donkeystones <oscar.back96@outlook.com>
2024-06-10 10:49:36 +0200

    2020 day 4 part 1 done

--- 2020/day_4/solution.js
+++ 2020/day_4/solution.js
@@ -3,16 +3,13 @@ let fs = require("fs");
 var data = fs.readFileSync('./input.txt','utf8');
 
 let data_arr = data.split("\n\n");
-let valid_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"];
+let valid_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 let sum = 0;
 for(let i = 0; i < data_arr.length; i++){
-	let valid = true;
 	let pass_data = data_arr[i].split(/[\n\s]/);//Part 2?
 	if(valid_fields.every(item => data_arr[i].includes(item))) {
 		sum++;
 	}
-
-	//Some comment
 }
 
 console.log(sum);
